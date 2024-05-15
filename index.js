82d201afc682e03c35505f4d2f30919b78df2f85
Bhaba34 <bhabaranjanpatra@5gmail.com>
2024-05-15 17:04:00 +0530

    files commits

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.u0ttgco.mongodb.net/registrationformDB`,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+});
+
+const registrationschema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String
+});
+ const Registration = mongoose.model("Registration",registrationschema);
+ app.use(bodyParser.urlencoded({ extended: true}));
+ app.use(bodyParser.json());
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/pages/index.html");
+});
+app.post("/register",async(req,res)=>{
+    try{
+        const {name,email,password} = req.body;
+        const existinguser = await Registration.findOne({email: email});
+        if(!existinguser){
+            const registrationDta = new Registration({
+                name,
+                email,
+                password
+            });
+            await registrationDta.save();
+            res.redirect("/success");
+
+        }
+        else{
+            console.log("user already exist");
+            res.redirect("/error");
+        }
+        
+        
+    }
+    catch (error){
+        console.log(error);
+        res.redirect("error");
+    }
+});
+
+app.get("/success",(req,res)=>{
+    res.sendFile(__dirname+"/pages/success.html");
+});
+
+app.get("/error",(req,res)=>{
+    res.sendFile(__dirname+"/pages/error.html");
+});
+
+app.listen(port,()=>{
+    console.log(`the server is running on port${port}`);
+})
